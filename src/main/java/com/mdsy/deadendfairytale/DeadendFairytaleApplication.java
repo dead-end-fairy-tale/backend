9e5ca4fb346e47631a7fbf88474cbd7dea491e50
smg0218 <smg000218@gmail.com>
2025-06-17 10:41:01 +0900

    spring boot 기본 파일 생성

---
+++ src/main/java/com/mdsy/deadendfairytale/DeadendFairytaleApplication.java
@@ -0,0 +1,13 @@
+package com.mdsy.deadendfairytale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadendFairytaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeadendFairytaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdsy/deadendfairytale/DeadendFairytaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsy.deadendfairytale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadendFairytaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
