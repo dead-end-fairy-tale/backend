c50c9ec179edd728bb287e195b00a983b5dc061c
smg0218 <smg000218@gmail.com>
2025-06-18 10:38:30 +0900

    chore : 토큰이 유효하지 않은 경우 반환값 수정이 적용되지 않던 문제 해결

--- src/main/java/com/mdsy/deadendfairytale/config/SecurityConfig.java
+++ src/main/java/com/mdsy/deadendfairytale/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.mdsy.deadendfairytale.config;
 
 import com.mdsy.deadendfairytale.filter.JwtAuthFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,7 +10,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +26,25 @@ public class SecurityConfig {
     public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
         this.jwtAuthFilter = jwtAuthFilter;
     }
+
+    private final Http403ForbiddenEntryPoint authenticationEntryPoint = new Http403ForbiddenEntryPoint() {
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException {
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            Map<String, Object> responseData = new HashMap<>();
+            responseData.put("status", false);
+            responseData.put("message", "인증이 필요합니다.");
+
+            if (request.getRequestURI().startsWith("/api")) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write(new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(responseData));
+                response.getWriter().flush();
+                response.getWriter().close();
+            }
+        }
+    };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -34,6 +60,9 @@ public class SecurityConfig {
                         .requestMatchers("/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception -> {
+                    exception.authenticationEntryPoint(authenticationEntryPoint);
+                })
                 .build();
     }
 }
