f19b51b263b39d2df31261c2f60e9585d54f735a
smg0218 <smg000218@gmail.com>
2025-06-25 16:53:18 +0900

    build : cicd 테스트 커밋2

--- src/main/java/com/mdsy/deadendfairytale/config/SwaggerConfig.java
+++ src/main/java/com/mdsy/deadendfairytale/config/SwaggerConfig.java
@@ -23,7 +23,7 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .info(new Info().title("막장동화 API")
-                        .description("막장동화 API 문서입니다.")
+                        .description("막장동화 API 문서 입니다.")
                         .version("v1.0"))
                 .addSecurityItem(securityRequirement)  // Security Requirement 추가
                 .schemaRequirement("BearerAuth", securityScheme);  // Security Scheme 추가
